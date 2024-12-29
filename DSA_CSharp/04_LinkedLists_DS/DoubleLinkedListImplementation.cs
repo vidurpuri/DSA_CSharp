3d3a28c48f7e5f982bcd4e648d2b9ca91ab10f1b
vidurpuri <vidur0610.puri@gmail.com>
2024-12-29 13:29:33 +0530

    DoubleLinked List

--- DSA_CSharp/04_LinkedLists_DS/DoubleLinkedListImplementation.cs
+++ DSA_CSharp/04_LinkedLists_DS/DoubleLinkedListImplementation.cs
@@ -1,5 +1,4 @@
-﻿using DSA_CSharp._04_LinkedLists_DS;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,63 +6,75 @@ using System.Threading.Tasks;
 
 namespace DSA_CSharp._04_LinkedLists_DS
 {
-    public class Node
+    public class DoublyNode
     {
         public int value { get; set; }
-        public Node next { get; set; }
+        public DoublyNode next { get; set; }
+        public DoublyNode previous { get; set; }
 
 
-        public Node(int value)
+        public DoublyNode(int value)
         {
             this.value = value;
             this.next = null;
+            this.previous = null;
         }
 
     }
-
-
-    public class LinkedListImplementation   
+    public class DoubleLinkedListImplementation
     {
-        public Node Head { get; set; }
-        public Node Tail { get; set; }
+        private DoublyNode Head { get; set; }
+        private DoublyNode Tail { get; set; }
+        private int length;
 
-        public int length;
-
-        public LinkedListImplementation(int value)
+        public DoubleLinkedListImplementation(int value)
         {
-            this.Head = new Node(value);
+            this.Head = new DoublyNode(value);
             this.Tail = this.Head;
-            this.length = 1;
+            this.length++;
         }
 
-        //[10,5,20]
+        public void PrintData()
+        {
+            if (this.Head == null)
+                return;
+            var node = this.Head;
+            while (node != null)
+            {
+                Console.Write(node.value + "--->");
+                node = node.next;
+            }
+            Console.WriteLine();
+        }
+
+        //[3,5,6 --> 7]
         //Append
         public void Append(int value)
         {
-            var newNode = new Node(value);
+            var newNode = new DoublyNode(value);
+            newNode.previous = this.Tail;
             this.Tail.next = newNode;
             this.Tail = newNode;
             this.length++;
             
         }
 
-        //[2 --> 10,5,20]
         //PrePend
+        //[1 --> 3,5,6]
         public void Prepend(int value)
         {
-            var newNode = new Node(value);
+            var newNode = new DoublyNode(value);
             newNode.next = this.Head;
+            this.Head.previous = newNode;
             this.Head = newNode;
             this.length++;
         }
 
-        //[10,5,20]
-        //Traverse
-        private Node TraverseToIndex(int index)
+        private DoublyNode TraverseToIndex(int index)
         {
             int counter = 0;
             var currentNode = this.Head;
-            while(counter != index)
+            while (counter != index)
             {
                 currentNode = currentNode.next;
                 counter++;
@@ -71,103 +82,70 @@ namespace DSA_CSharp._04_LinkedLists_DS
             return currentNode;
         }
 
-        //[10,5,15,20] index 1, 8
-        // 10 --> 8 --> 5
-        //Insert
+        //[3,5,6] index =1 , value 7 ---> New Node [3,5,7,6]
         public void Insert(int index, int value)
         {
-            //If first item to insert
             if(index == 0)
                 this.Prepend(value);
-
-            //if last item to insert
-            if(index >= this.length)
+            if(index > this.length)
                 this.Append(value);
 
-            //Insert in between list
-            if(index > 0 && index < this.length)
-            {
-                var newNode = new Node(value);
-                var leader = this.TraverseToIndex(index - 1);
-                var nodeToBePushed = leader.next;
-                leader.next = newNode;
-                newNode.next = nodeToBePushed;
-                this.length++;
-            }
+            var newNode = new DoublyNode(value);
+            var leader = this.TraverseToIndex(index - 1);
+            var follower = leader.next;
+            leader.next = newNode;
+            newNode.previous = leader;
+            newNode.next = follower;
+            follower.previous = newNode;
+            this.length++;
+
         }
 
-        //[5,10,15]
-        //Remove
+        //[3,5,6,7,8] index = 2 --> New Node = [3,5,7,8]
         public void Remove(int index)
         {
             //First Element to remove 
-            if(index == 0)
+            if (index == 0)
             {
                 this.Head = Head.next;
             }
-
-
             //Last element to remove
-            if(index >= this.length)
+            if (index >= this.length)
             {
                 var leade = this.TraverseToIndex(index - 1);
                 this.Tail = leade;
                 this.length--;
             }
-
-
             //in between node to be removed 
             var leader = this.TraverseToIndex(index - 1);
             var nodeToBeRemoved = leader.next;
-            leader.next = nodeToBeRemoved.next;
+            var follwer = nodeToBeRemoved.next;
+            leader.next = follwer;
+            follwer.previous = leader;
             this.length--;
         }
+
         //[2,5,3,7,5]
         public void Reverse()
         {
             // Start of list Grab Head Node i.e 2
-            var current = this.Head; 
+            var current = this.Head;
             // Previous value be null initally
-            Node previous = null;
+            DoublyNode previous = null;
 
             //loop through till current has value
             while (current != null)
             {
                 var temp = current.next; // Grab Second node
                 current.next = previous; // Break pointer of 1 --> 2 and point 1 --> prvious
+                current.previous = previous;
                 previous = current; // Now reverse logic to point previous to current as in baove step current became previous
                 current = temp; // to continue looping current has to be moved to nect node  
             }
             // as above logic goes reverse everything by pointing list in reverse , we just need to point tail to head 
             // so that we can grab full list.
-            this.Head = this.Tail;  
+            this.Head = this.Tail;
 
-        }
-
-
-        //printData
-        public void PrintData()
-        {
-            if (this.Head == null)
-                return;
-            var node = this.Head;
-            while(node != null)
-            {
-                Console.Write(node.value+"--->" );
-                node = node.next;
-            }
-            Console.WriteLine();
         }
     }
 }
-
-
-//Void Main Method call
-
-//LinkedListImplementation linkedList = new(10);
-//linkedList.Append(5);
-//linkedList.Append(2);
-//linkedList.Prepend(3);
-//linkedList.Insert(2, 25);
-//linkedList.Remove(2);
-//linkedList.PrintData();
--- DSA_CSharp/Program.cs
+++ DSA_CSharp/Program.cs
@@ -5,14 +5,18 @@ using DSA_CSharp._02_Arrays_DS;
 using DSA_CSharp._03_HashTables_DS;
 using DSA_CSharp._04_LinkedLists_DS;
 
-LinkedListImplementation linkedList = new(10);
+DoubleLinkedListImplementation linkedList = new(10);
 linkedList.Append(5);
 linkedList.Append(2);
-linkedList.Prepend(3);
-linkedList.Insert(2, 25);
-linkedList.Remove(2);
-linkedList.PrintData();
+linkedList.Append(1);
+linkedList.Append(6);
 
+//linkedList.Prepend(3);
+//linkedList.Insert(2, 25);
+//linkedList.Remove(2);
+linkedList.PrintData();
+linkedList.Reverse();
+linkedList.PrintData();
 
 
 
